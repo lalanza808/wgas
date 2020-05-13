2c066fc93482af80ca5701d1ee23dc4de7191f69
lalanza808 <lance@lzahq.tech>
2020-05-13 00:53:36 -0700

    use Strings and return tuple for wg_cmd

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,18 +1,20 @@
 use std::process::Command;
 
 
-pub fn wg_cmd(args: &'static [&'static str]) -> String {
+pub fn wg_cmd(args: Vec<String>) -> (String, i32) {
     let output = Command::new("./bin/wg_cmd")
         .args(args)
         .output()
         .expect("failed to execute process");
     let output_str = String::from_utf8(output.stdout)
         .unwrap();
+    let resp_code = output.status.code()
+        .unwrap();
 
-    output_str
+    (output_str, resp_code)
 }
 
-pub fn sh_cmd(cmd: &'static str) -> String {
+pub fn sh_cmd(cmd: String) -> String {
     let output = Command::new("sh")
         .arg("-c")
         .arg(cmd)
